3c62386eef9b7879d4aaba0da284486a0dd5cf67
Jatez95 <mateossanchezjavier@gmail.com>
2025-03-12 11:14:43 +0100

    Files moved to subfolder for better understanding

---
+++ python-version/src/main.py
@@ -0,0 +1,11 @@
+from dbtables.recipes import Recipes
+from dbtables.users import Users
+from dbtables.comments import Comment
+
+def main():
+    table_query = Comment()
+
+    table_query.delete_comment(id_user = "0965c71f-d833-45e1-8a3a-2b78f6021817", id_comment = "e443c3cd-d4bd-45c3-adfc-d365a34765c9")
+
+if __name__ == '__main__':
+    main()
